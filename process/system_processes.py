49af99a334f7323497113b4e6fa4c93acdf33cc6
Venomsec45 <dayshaunmarnin@gmail.com>
2026-04-01 16:40:31 +0800

    To check remaining processes and their usage

---
+++ process/system_processes.py
@@ -0,0 +1,34 @@
+import psutil
+from rich.table import Table
+from rich.text import Text
+from colors import custom_colors
+
+
+def processes():
+    table = Table(expand=True)
+    table.add_column("PID")
+    table.add_column("Process")
+    table.add_column("Usage")
+
+    processes = [
+        process.info
+        for process in psutil.process_iter(["pid", "name", "cpu_percent"])
+    ]
+
+    top_processes = sorted(
+        processes,
+        key=lambda a: a["cpu_percent"],
+        reverse=True
+    )
+
+    for process in top_processes:
+        if process["cpu_percent"] >= 80:
+            table.add_row(str(process["pid"]), str(process["name"]), Text(f"{process["cpu_percent"]}%", style=custom_colors()[2]))
+        
+        elif process["cpu_percent"] >= 48:
+            table.add_row(str(process["pid"]), str(process["name"]), Text(f"{process["cpu_percent"]}%", style=custom_colors()[1]))
+
+        else:
+            table.add_row(str(process["pid"]), str(process["name"]), Text(f"{process["cpu_percent"]}%", style=custom_colors()[0]))        
+
+    return table
