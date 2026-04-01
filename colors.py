600e6d97ef0f430be73bfc28b68f1592d74b906f
Venomsec45 <dayshaunmarnin@gmail.com>
2026-04-01 16:30:32 +0800

    Added custom colors

---
+++ colors.py
@@ -0,0 +1,5 @@
+def custom_colors():
+    custom_green_color = "rgb(103,245,10)"
+    custom_yellow_color = "rgb(245,232,7)"
+    custom_red_color = "rgb(247,7,7)"
+    return [custom_green_color, custom_yellow_color, custom_red_color]
