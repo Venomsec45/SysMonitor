b09f8df9ca8a31c6750ddbdf99082e7fd47ecd23
Venomsec45 <dayshaunmarnin@gmail.com>
2026-04-04 22:15:32 +0800

    Removed the text word in the display

--- disk/disk_status.py
+++ disk/disk_status.py
@@ -5,7 +5,7 @@ from disk.disk_usage import disk_usage
 
 def disk():
     return Group(
-        Text("Total disk capacity: ").append(f"{disk_usage()[0]}", style=custom_colors()[0]),
-        Text("Used disk capacity: ").append(f"{disk_usage()[1]}", style=custom_colors()[0]),
-        Text("Free disk capacity: ").append(f"{disk_usage()[2]}", style=custom_colors()[0])
+        Text("Total capacity: ").append(f"{disk_usage()[0]}", style=custom_colors()[0]),
+        Text("Used capacity: ").append(f"{disk_usage()[1]}", style=custom_colors()[0]),
+        Text("Free capacity: ").append(f"{disk_usage()[2]}", style=custom_colors()[0])
     )
