ee5e0b1e72edc019cf844f013474299d1d671713
Venomsec45 <dayshaunmarnin@gmail.com>
2026-04-04 09:03:52 +0800

    New python file added to show network adapters

--- network/network_adapters.py
+++ network/network_adapters.py
@@ -2,11 +2,11 @@ import netifaces
 from rich.table import Table
 
 def net_adapters():
-    table = Table(show_header=False, box=None)
+    table = Table(show_header=True, box=None)
     table.add_column("NUMBER")
     table.add_column("ADAPTER")
 
     for index, net_adapter in enumerate(netifaces.interfaces(), start=1):
-        table.add_row(index, net_adapter)
+        table.add_row(str(index), str(net_adapter))
 
     return table
