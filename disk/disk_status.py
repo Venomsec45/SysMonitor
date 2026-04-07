7f6cd68b8412ef4cc26c68b90bdfcb68cf3bfd6a
Venomsec45 <dayshaunmarnin@gmail.com>
2026-04-07 14:18:20 +0800

    Lines of code fixed

--- disk/disk_status.py
+++ disk/disk_status.py
@@ -4,18 +4,22 @@ from rich.text import Text
 from rich.table import Table
 from colors import custom_colors
 from disk.disk_usage import disk_usage
+from disk.disk_partitions_usage import partitions_usage
 
 def disk():
     table = Table(expand=True, show_header=True, box=None)
     table.add_column("DISK")
-    table.add_column("MOUNTPOINT")
+    table.add_column("USAGE")
 
     for disk in psutil.disk_partitions():
-        table.add_row(disk.device, disk.mountpoint)
+        disk_partition_usage = psutil.disk_usage(disk.mountpoint)
+        table.add_row(disk.device, f"[{custom_colors()[0]}]{str(disk_partition_usage.percent)}%[/{custom_colors()[0]}]")
 
     return Group(
         Text("Total capacity: ").append(f"{disk_usage()[0]}", style=custom_colors()[0]),
         Text("Used capacity: ").append(f"{disk_usage()[1]}", style=custom_colors()[0]),
-        Text("Free capacity: ").append(f"{disk_usage()[2]}\n", style=custom_colors()[0]),
+        Text("Free capacity: ").append(f"{disk_usage()[2]}", style=custom_colors()[0]),
+        Text("Read bytes: ").append(f"{partitions_usage()[0]}", style=custom_colors()[0]),
+        Text("Write bytes: ").append(f"{partitions_usage()[1]}\n", style=custom_colors()[0]),
         table
     )
