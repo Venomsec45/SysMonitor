0a7a3b89f25d892fbcebb6d62695c920c4687fa8
Venomsec45 <dayshaunmarnin@gmail.com>
2026-04-07 14:20:45 +0800

    New file added for showing read and write output

--- disk/disk_partitions_usage.py
+++ disk/disk_partitions_usage.py
@@ -1,10 +1,9 @@
 import psutil
 
 def partitions_usage():
-    old_pr_usage = psutil.disk_io_counters()
-    new_pr_usage = psutil.disk_io_counters()
+    pr_usage = psutil.disk_io_counters()
     
     return [
-        f"{new_pr_usage.read_bytes - old_pr_usage.read_bytes} %",
-        f"{new_pr_usage.write_bytes - old_pr_usage.write_bytes} %"
+        f"{pr_usage.read_bytes}",
+        f"{pr_usage.write_bytes}"
     ]
