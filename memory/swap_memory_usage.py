94f04ccf9b91cc5d73027284b872a482778f63ce
Venomsec45 <dayshaunmarnin@gmail.com>
2026-04-07 14:22:31 +0800

    New file for showing swap memory usage

--- memory/swap_memory_usage.py
+++ memory/swap_memory_usage.py
@@ -1,8 +1,28 @@
 import psutil
 
 def swap_memory():
-    return [
-        f"{psutil.swap_memory().total}",
-        f"{psutil.swap_memory().used}",
-        f"{psutil.swap_memory().free}"
-    ]
+    sw_memory = psutil.swap_memory()
+    gigabytes = 1024 ** 3
+    megabytes = 1024 ** 2
+    kilobytes = 1024
+
+    if any(mem >= gigabytes for mem in [sw_memory.total, sw_memory.used, sw_memory.free]):
+        return [
+            f"{psutil.swap_memory().total / gigabytes:.2f} GB",
+            f"{psutil.swap_memory().used / gigabytes:.2f} GB",
+            f"{psutil.swap_memory().free / gigabytes:.2f} GB"
+        ]
+    
+    elif any(mem >= megabytes for mem in [sw_memory.total, sw_memory.used, sw_memory.free]):
+        return [
+            f"{psutil.swap_memory().total / megabytes:.2f} MB",
+            f"{psutil.swap_memory().used / megabytes:.2f} MB",
+            f"{psutil.swap_memory().free / megabytes:.2f} MB"
+        ]
+    
+    else:
+        return [
+            f"{psutil.swap_memory().total / kilobytes:.2f} KB",
+            f"{psutil.swap_memory().used / kilobytes:.2f} KB",
+            f"{psutil.swap_memory().free / kilobytes:.2f} KB"
+        ]
