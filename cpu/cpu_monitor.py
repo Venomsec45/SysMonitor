ff1159f5fd1785d6c0ce24479584af904f2ba91e
Venomsec45 <dayshaunmarnin@gmail.com>
2026-04-07 14:15:52 +0800

    Few adjustments and fixes

--- cpu/cpu_monitor.py
+++ cpu/cpu_monitor.py
@@ -4,12 +4,14 @@ from rich.console import Group
 from colors import custom_colors
 from cpu.cpu_usage import usage
 
-
 def monitor():
     return Group(
-        Text(f"CPU Usage: ").append(f"{psutil.cpu_percent(interval=1)}%", style=usage(psutil.cpu_percent(interval=1))),
-        Text(f"CPU Switches: ").append(f"{psutil.cpu_stats().ctx_switches}", style=custom_colors()[0]),
-        Text(f"CPU Interrupts: ").append(f"{psutil.cpu_stats().interrupts}", style=custom_colors()[0]),
-        Text(f"CPU Soft interrupts: ").append(f"{psutil.cpu_stats().soft_interrupts}", style=custom_colors()[0]),
-        Text(f"CPU Syscalls: ").append(f"{psutil.cpu_stats().syscalls}", style=custom_colors()[0])
+        Text("Usage: ").append(f"{psutil.cpu_percent(interval=1)}%", style=usage(psutil.cpu_percent(interval=1))),
+        Text("Switches: ").append(f"{psutil.cpu_stats().ctx_switches}", style=custom_colors()[0]),
+        Text("Interrupts: ").append(f"{psutil.cpu_stats().interrupts}", style=custom_colors()[0]),
+        Text("Soft interrupts: ").append(f"{psutil.cpu_stats().soft_interrupts}", style=custom_colors()[0]),
+        Text("Syscalls: ").append(f"{psutil.cpu_stats().syscalls}\n", style=custom_colors()[0]),
+        Text("User: ").append(f"{psutil.cpu_times().user}", style=custom_colors()[0]),
+        Text("System: ").append(f"{psutil.cpu_times().system}", style=custom_colors()[0]),
+        Text("Idle: ").append(f"{psutil.cpu_times().idle}", style=custom_colors()[0])
     )
